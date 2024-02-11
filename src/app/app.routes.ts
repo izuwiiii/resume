04eb6c91c0989b721c02d1c8d2cd74428d6eca6c
izuwi <freefrip@gmail.com>
2024-02-11 17:46:24 +0200

    d

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'main', pathMatch: 'full' },
+    // { path: '', redirectTo: 'main', pathMatch: 'full' },
+    { path: '', component: MainComponent },
     { path: 'main', component: MainComponent },
     { path: '**', component: MainComponent }
 ];
