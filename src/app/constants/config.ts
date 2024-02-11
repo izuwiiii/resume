9b02a7101d884fcd1e1ca562f05ee14d62eb4098
izuwi <freefrip@gmail.com>
2024-02-11 19:35:06 +0200

    img

--- src/app/constants/config.ts
+++
@@ -1,3 +0,0 @@
-export const ME_URL = '/assets/images/me.png'
-
-export const LOGO = './assets/images/ang.png'
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -3,7 +3,7 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 // import function to register Swiper custom elements
 import { register } from 'swiper/element/bundle';
-import { ME_URL } from '../../constants/config';
+
 // register Swiper custom elements
 register();
 
@@ -19,7 +19,5 @@ register();
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-
-    meURL: string = ME_URL
 
 }
